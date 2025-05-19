f1179c85ee6dbb40c8779a54afd72d78969d3805
vitorteodoro <teodorovitor30@gmail.com>
2025-05-19 10:34:16 -0300

    Desafio da lista de seleção

--- desafio.py
+++ desafio.py
@@ -1,14 +1,35 @@
-def funcao1 (a:int,b:int)->bool:
-    c=a-b
-    return(c)
+def caracteres_exibidos (mensagem:str,momento:int,numero_caracteres:int):
+    
+    
+    '''
+    >>> caracteres_exibidos('Promocao de sorvetes, pague 2 leve 3!',4,13)
+    ocao de sorve
+    >>> caracteres_exibidos('Promocao de sorvetes, pague 2 leve 3!',32,13)
+    ve 3! Promoca
+    >>> caracteres_exibidos('Promocao de sorvetes, pague 2 leve 3!',35,13)
+    3! Promocao d
+    >>> caracteres_exibidos('Promocao de sorvetes, pague 2 leve 3!',4,25)
+    ocao de sorvetes, pague 2
+    >>> caracteres_exibidos('Promocao de sorvetes, pague 2 leve 3!',15,25)
+    vetes, pague 2 leve 3! Pr
+    >>> caracteres_exibidos('Promocao de sorvetes, pague 2 leve 3!',29,25)
+     leve 3! Promocao de sorv
+    '''
+    if momento<=len(mensagem)-numero_caracteres:
+        exibir=mensagem[momento:momento+numero_caracteres]
+    else:
+        exibir=mensagem[momento:]+' '+mensagem[:numero_caracteres-(len(mensagem)-momento+1)]
+
+    
+    
+    
+    return print(exibir)
 
 
-def qual_maior (a:int,b:int)->bool:
-    return print("O número ",a," é o maior?",funcao1(a,b)>0,"\n O número ",b," é o maior?",funcao1(a,b)<0)
 
-
-qual_maior(1,2)
-qual_maior(0,8)
-qual_maior(177,94)
-qual_maior(-6,-3)
-qual_maior(-2,6)
+caracteres_exibidos('Promocao de sorvetes, pague 2 leve 3!',4,13)
+caracteres_exibidos('Promocao de sorvetes, pague 2 leve 3!',32,13)
+caracteres_exibidos('Promocao de sorvetes, pague 2 leve 3!',35,13)
+caracteres_exibidos('Promocao de sorvetes, pague 2 leve 3!',4,25)
+caracteres_exibidos('Promocao de sorvetes, pague 2 leve 3!',15,25)
+caracteres_exibidos('Promocao de sorvetes, pague 2 leve 3!',29,25)

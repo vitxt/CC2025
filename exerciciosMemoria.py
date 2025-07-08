22ee000e4849dc2dac108bf9a2d2145e3a93128d
vitorteodoro <teodorovitor30@gmail.com>
2025-07-08 14:50:39 -0300

    Update exerciciosMemoria.py

--- exerciciosMemoria.py
+++ exerciciosMemoria.py
@@ -41,7 +41,9 @@ def newLista(a:list,b:list)->list:
         a.append(b[i])
     return a
 
-def insere(lst:list,index:int,valor):
+
+
+def insere(lst:list,valor,index:int):
     '''
     Projete uma função que receba como parâmetros uma lista, um índice i e um valor v, e modifique a
     lista inserindo o valor v no índice i. Dica: veja o exemplo insere_ordenado.
@@ -51,26 +53,102 @@ def insere(lst:list,index:int,valor):
     [0, 1, 2, 3, 4, 5]
 
     >>> a = [10, 20, 30]
-    >>> insere_em(a, 15, 1)
-    >>> a
+    >>> insere(a,15,1)
     [10, 15, 20, 30]
 
     >>> b = []
-    >>> insere(b, 0, 'a')
+    >>> insere(b,'a',0)
     ['a']
 
     >>> c = [1, 2, 3]
-    >>> insere(c, 4, 4)
+    >>> insere(c,4,3)
     [1, 2, 3, 4]
 
   
     '''
     lst.append(valor)
-    i = len(lst) - 1
-    while i != 0:
-        if lst[index]!=valor:
-            aux = lst[i]
-            lst[i] = lst[i-1]
-            lst[i-1] = aux
-        i = i -1
+    i = len(lst)-1
+    certo = False
+    while i > 0 and not certo:
+        aux = lst[i]
+        if lst[i] == valor and i == index:
+            certo = True
+        else:
+            lst[i]=lst[i-1]
+            lst[i-1]=aux
+        i = i-1
+
+    return lst
+
+def remove_indice(lst:list,index:int):
+    '''Projete uma função que receba como parâmetros uma lista e um índice i e modifique a lista removendo
+    o elemento do índice i.
+    >>> lst = [7, 1, 8, 9]
+    >>> remove_indice(lst, 2)
+    [7, 1, 9]
+
+
+    >>> lst2 = ['a', 'b', 'c', 'd', 'e']
+    >>> remove_indice(lst2, 0)
+    ['b', 'c', 'd', 'e']
+
+    >>> lst3 = [10, 20, 30]
+    >>> remove_indice(lst3, 2)
+    [10, 20]
+
+    >>> lst4 = [5]
+    >>> remove_indice(lst4, 0)
+    []
+
+    '''
+    while index < len(lst)-1:
+        aux = lst[index]
+        lst[index] = lst[index+1]
+        lst[index+1]=aux
+        index +=1
+    lst.pop()
+    return lst
+
+
+def remover_indices_pares(lst:list):
+    '''Projete uma função que modifique uma lista removendo todos os elementos que estão em índices pares
+    (não utilize uma lista auxiliar)
+
+
+    >>> lista2 = ['a', 'b', 'c', 'd']
+    >>> remover_indices_pares(lista2)
+    ['b', 'd']
+
+    >>> lista3 = [10, 20, 30]
+    >>> remover_indices_pares(lista3)
+    [20]
+
+    >>> lista4 = [100]
+    >>> remover_indices_pares(lista4)
+    []
+
+    >>> lista5 = []
+    >>> remover_indices_pares(lista5)
+    []
+
+    >>> lista6 = [1, 2]
+    >>> remover_indices_pares(lista6)
+    [2]
+
+    >>> lista1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    >>> remover_indices_pares(lista1)
+    [1, 3, 5, 7, 9]
+    '''
+    a = len(lst)-1
+    while a > 0:
+        if a % 2 == 0:
+            index = a
+            while index < len(lst)-1:
+                aux = lst[index]
+                lst[index] = lst[index+1]
+                lst[index+1]=aux
+                index +=1
+                lst.pop()
+        a -=1
+
     return lst
